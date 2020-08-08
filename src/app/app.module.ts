d047ceb077cde75703c5c39831aa53b46102cf79
nobodyme <nobodydisturbme@gmail.com>
2020-08-08 17:50:33 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FeatureListComponent } from './feature-list/feature-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FeatureListComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
