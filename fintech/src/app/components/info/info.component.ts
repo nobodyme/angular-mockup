2a830320eab06b110855f6e4b7ca3c9b2bd6427a
nobodyme <nobodydisturbme@gmail.com>
2020-08-23 19:06:27 +0530

    chart added

--- fintech/src/app/components/info/info.component.ts
+++ fintech/src/app/components/info/info.component.ts
@@ -15,4 +15,11 @@ export class InfoComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getRandomInt(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+    // The maximum is exclusive and the minimum is inclusive
+  }
+
 }
